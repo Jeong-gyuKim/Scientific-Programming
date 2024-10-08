9ec3636819456d9cedf351999ab7ad6536aeacd2
Jeong-gyuKim <jgsteve104@gmail.com>
2024-10-08 10:25:28 +0900

    well working buffonWithoutPi

--- buffon.py
+++ buffon.py
@@ -1,45 +1,65 @@
+"""
+Project Name    :Scientific Programming HW
+
+File Name       :buffon.py
+Date            :2024.09.25
+Author          :Jeong-gyu, Kim
+"""
+
 import numpy as np
-import matplotlib.pyplot as plt
 
-def buffonWithPi(n,l,d):
-    '''
-    n: 시행 횟수
-    l: 바늘 길이
-    d: 평행선 간격
-    '''
-
+def buffonWithPi(n,d):#get random value with reference value of pi
     x_random = np.random.rand(n) * (d / 2)
     theta_random = np.random.rand(n) * np.pi
+    
+    return x_random, theta_random
 
+def buffonWithoutPi(n,d):#get random value without reference value of pi
+    x_random = np.random.rand(n) * (d / 2)
+    
+    x, y = np.zeros(n), np.zeros(n)
+    for i in range(n):
+        while 1:
+            X, Y = np.random.rand(), np.random.rand()
+            if X**2 + Y**2 <= 1 :
+                break
+        x[i], y[i] = X, Y
+    theta_random = np.arctan2(x,y)
+    
+    return x_random, theta_random
+
+def buffon(n,l,d,f):#길이 l짜리 바늘을 간격 d의 평행선 사이에 n번 던져서 pi값을 계산한다.
+    cnt = 1
+
+    x_random, theta_random = f(n,d)
     touches = x_random < (l / 2) * np.sin(theta_random)
-    cnt = np.sum(touches)
+    cnt += np.sum(touches)
     
-    pi_estimate = (2 * n * l) / (cnt * d)
-    
+    if cnt:
+        pi_estimate = (2 * n * l) / (cnt * d)
+    else:
+        raise ValueError
     return pi_estimate
 
-def buffonWithoutPi(n,l,d):
-    '''
-    n: 시행 횟수
-    l: 바늘 길이
-    d: 평행선 간격
-    '''
-    
-    return 0
-
-def static(n,l,d,N=30,f=buffonWithPi):
-    li = [f(n,l,d) for _ in range(N)]
+def ErrStatic(li):#N회 buffon함수를 실행한 결과로 오차의 평균과 표준편차를 반환한다.
+    li = abs(li-np.pi)
     return np.mean(li), np.std(li)
 
-def diffReal(n_list, l, d, N=30, f=buffonWithPi):
-    M = np.zeros_like(n_list).astype(float)
+def diffrence(n_list, l, d, N=30, f=buffonWithoutPi):#n_list에 들어있는 n값마다 ErrStatic을 실행한 결과를 반환한다.
+    Err = np.zeros_like(n_list).astype(float)
+    dErr = np.zeros_like(n_list).astype(float)
+    Ns = np.zeros(len(n_list)*N)
+    Pi = np.zeros(len(n_list)*N)
     for i, n in enumerate(n_list):
-        m, s = static(n,l,d,N,f)
-        M[i] = m
-    plt.scatter((n_list),(abs(M-np.pi)))
-    plt.title("Buffon's needle")
-    plt.xlabel("log10(n)")
-    plt.ylabel("log10(Err)")
-    plt.loglog()
-    plt.show()
-    return M
+        li = np.array([buffon(n,l,d,f) for _ in range(N)])
+        Err[i], dErr[i]= ErrStatic(li)
+        for j in range(N):
+            Ns[i*N+j] = n
+            Pi[i*N+j] = li[j]
+    return Err, dErr, Ns, Pi
+
+def LinearRegression(x,y):#입력된 데이터에 y=a*x+b를 fitting한다.
+    x, y = np.array(x), np.array(y)
+    a = sum((x-np.mean(x))*(y-np.mean(y)))/sum((x-np.mean(x))**2)
+    b = np.mean(y) - a * np.mean(x)
+    return a, b
--- main.py
+++ main.py
@@ -1,9 +1,50 @@
-from buffon import buffonWithPi, buffonWithoutPi, static, diffReal
+"""
+Project Name    :Scientific Programming HW
+
+File Name       :main.py
+Date            :2024.09.25
+Author          :Jeong-gyu, Kim
+"""
+
+from buffon import diffrence, LinearRegression
 import numpy as np
 import matplotlib.pyplot as plt
 
-n=10**7
+################################################################
+#input parameter
 l,d = 1,1
 n_list = (10**np.arange(1,6,0.1)).astype(int)
 
-diffReal(n_list,l,d, N=300)
+################################################################
+#calculate error
+err, derr, Ns, Pi = diffrence(n_list,l,d,N=30)
+a,b = LinearRegression(np.log(n_list), np.log(err))
+
+################################################################
+#draw graph
+#Figure_1
+plt.figure()
+plt.scatter(n_list, err, label="Buffon's needle")
+plt.fill_between(n_list, err - derr, err + derr, alpha=.25)
+plt.plot(n_list, np.exp(a*np.log(n_list)+b), label="linear regression\n(slope={})".format(round(a,3)))
+
+plt.title("Result of the Scientific Programming HW")
+plt.xlabel("# of throws")
+plt.ylabel("Error")
+
+plt.legend()
+plt.loglog()
+
+plt.savefig("Figure_1.png")
+
+#Figure_2
+plt.figure()
+plt.scatter(np.log10(Ns), Pi, label="Buffon's needle", alpha=0.3)
+plt.plot([1,6],[np.pi,np.pi],color='r',label="Pi")
+
+plt.title("Result of the Scientific Programming HW")
+plt.xlabel("log(# of throws)")
+plt.ylabel("Estimated Pi")
+
+plt.legend()
+plt.savefig("Figure_2.png")
